9d2c6019b4dd33046c8261d23cdab1b26e5590f6
fabulouscode <ulaghaondo@gmail.com>
2020-08-06 19:39:07 -0700

    Model -a created

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+//CRUD route controller
+Route::apiResource('/categories', 'CategoryController');
+
+Route::apiResource('/products', 'ProductController');
+
+Route::group(['prefix' => 'products'], function () {
+    Route::apiResource('/{products}/review', 'ReviewController');
+});
+
