a250da79e51b43f36390dea3d10920f793de76e1
fabulouscode <ulaghaondo@gmail.com>
2020-08-07 14:44:38 -0700

    Product details modified

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -16,10 +16,15 @@ class ProductResource extends JsonResource
     {
         return [
             'name' => $this->name,
-            'details' => $this->detail,
+            'details' => $this->details,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => round((1-($this->discount/100)) * $this->discount, 2),
+            'rating' => $this->reviews->count() > 0 ?round($this->reviews->sum('star')/$this->reviews->count(), 2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('review.index', $this->id)
+            ]
         ];
     }
 }
